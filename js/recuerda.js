4558b9834cba7996e57586104973cf7be68ae57b
erodriguezmartinez <erodriguezmartinez.guadalupe@alumnado.fundacionloyola.net>
2021-10-12 23:26:24 +0200

    Commint Actualizado

---
+++ js/recuerda.js
@@ -0,0 +1,31 @@
+/*  ejer1.js
+  Ejercicio 1 de Arrays
+  Autor:Esperanza Rodríguez Martínez<erodriguezmartinez.guadalupe@alumnado.fundacionloyola.net>
+  Licencia:GPL v3 o superior
+  Año 2021
+
+1.- 🧠 Programar es PENSAR, no teclear.
+   2.- 💧 Gota a gota, programa un poco y prueba.
+    	Si depurar es quitar bugs del código, entonces programar es meterlos.
+   ...
+    8.- 🧻 Código limpio y comentado
+    9.- 💋 Keep It Simple, Stupid (KISS)
+	...
+    12.- 🧸 Nombres sencillos y descriptivos.
+    13.- 🐫🐍 Usa el case adecuado: camelCase, PascalCase, snake_case, kebab-case, ALLCASE, nonecase
+    	a.- Nombres de ficheros en nonecase.
+    	b.- Nombres de Variables en JavaScript en camelCase.
+    	c.- Nombres de Clase en PascalCase.
+    	d.- Nombres de Constantes en ALLCASE.
+    	e.- Marcas de HTML en nonecase.
+    14.- 💧🌕 Ni una sola gota de agua sobre la superficie de Arrakis.
+	...
+_____________________________________________________________________
+	a.- Enlazar los ficheros de estilo en el <head>.
+	b.- Cargar los ficheros de script al final del <body>
+
+  COMPAÑEROS:
+  mperezpizarro
+  MSG8
+  josefdzbeta
+*/
